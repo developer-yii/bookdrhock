7b6f4b034ea9784ff3ee7a9ceeca48aab82f1375
yiideveloper <yiicakephp@gmail.com>
2022-11-10 13:20:47 +0530

    js and css changes

--- public/widget/load.js
+++ public/widget/load.js
@@ -1,6 +1,5 @@
-domain_url = "http://127.0.0.1:8001";
-//domain_url = "https://www.bookdrhock.com";
-//domain_url = "https://bookdrhock.amcodr.co";
+//domain_url = "http://127.0.0.1:8001";
+domain_url = "https://dev.amcodr.co/bookdrhock/public";
 var d = new Date();
 var rand = d.getMilliseconds();
 if(typeof jQuery == 'undefined'){
@@ -9,14 +8,32 @@ if(typeof jQuery == 'undefined'){
     oScriptElem.nonce="r@nd0m";
     oScriptElem.src = domain_url+"/widget/jquery.js?"+rand;
     document.head.insertBefore(oScriptElem, document.head.getElementsByTagName("script")[0]);
+
+    oScriptElem.addEventListener("load", () => {
+        console.log("import js loaded");
+        if(typeof fandomz_widget_load=="undefined")
+        {
+            var oScriptElem = document.createElement("script");
+            oScriptElem.type = "text/javascript";
+            oScriptElem.src = domain_url+"/widget/widget.js?"+rand;
+            document.head.appendChild(oScriptElem, document.head.getElementsByTagName("script")[0]);
+        }
+    });
+    // error event
+    oScriptElem.addEventListener("error", (ev) => {
+        console.log("Error on loading import js", ev);
+    });
+}else{
+    console.log("site js loaded");
+    setTimeout(function () {
+        if(typeof fandomz_widget_load=="undefined")
+        {
+            var oScriptElem = document.createElement("script");
+            oScriptElem.type = "text/javascript";
+            oScriptElem.src = domain_url+"/widget/widget.js?"+rand;
+            document.head.appendChild(oScriptElem, document.head.getElementsByTagName("script")[0]);
+        }   
+    },500);
+
 }
-if(typeof jQuery == 'undefined'){
-    document.write('<script src="'+domain_url+'/widget/jquery.js"></script>');
-}
-if(typeof fandomz_widget_load=="undefined")
-{
-    var oScriptElem = document.createElement("script");
-    oScriptElem.type = "text/javascript";
-    oScriptElem.src = domain_url+"/widget/widget.js?"+rand;
-    document.head.appendChild(oScriptElem, document.head.getElementsByTagName("script")[0]);
-}
+
