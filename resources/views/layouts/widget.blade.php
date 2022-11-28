bcf561b96d95c963494a451d09cbb01639558154
yiideveloper <yiicakephp@gmail.com>
2022-11-28 13:02:01 +0530

    js and css minify changes

--- resources/views/layouts/widget.blade.php
+++ resources/views/layouts/widget.blade.php
@@ -1,11 +1,17 @@
-<?php $version = time(); ?>
+<?php $version = 20221128; ?>
+<meta name="robots" content="index"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
+<meta http-equiv="Pragma" content="no-cache"/>
+<meta http-equiv="Expires" content="0"/>
+
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link rel='preload' as='style' href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-<link href="{{ asset('plugins/toast-master/css/jquery.toast.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('widget/jquery.fancybox.min.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('widget/all_min.css') }}?<?=$version?>" rel="stylesheet" type="text/css">
 @stack('extraStyle')
-<link href="{{ asset('widget/custom_css.css') }}?<?=$version?>" rel="stylesheet" type="text/css">
 <script type="text/javascript">
     var $ = jQuery;
 </script>
@@ -24,11 +30,7 @@
     </div>
     @yield('content')    
 </div>
-<script src="{{ asset('plugins/toast-master/js/jquery.toast.js') }}" type="text/javascript"></script>    
-<script src="{{ asset('widget/jquery.lazyload.min.js') }}" type="text/javascript"></script>        
-<script src="{{ asset('widget/jquery.fancybox.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('widget/moment.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('widget/custom_js.js') }}?<?=$version?>" type="text/javascript"></script>
+<script src="{{ asset('widget/all_min_js.js') }}?<?=$version?>" type="text/javascript"></script>
 <script type="text/javascript">
     var recaptcha_key = "<?=(env('GOOGLE_RECAPTCHA_KEY') != "") ? env('GOOGLE_RECAPTCHA_KEY') : ""?>";
     var recaptcha_1="";
